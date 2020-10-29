ab8b373450247bd8ee0804818b1ffdf093cc5765
raphaells <raphaelbn10@gmail.com>
2020-10-28 22:53:29 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import firebase from "firebase";
 import router from "./router";
 import store from "./store";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAb1wdhDzgGAh19CwTmKXDBbJR-VwDJqYs",
+  authDomain: "my-place-f144b.firebaseapp.com",
+  databaseURL: "https://my-place-f144b.firebaseio.com",
+  projectId: "my-place-f144b",
+  storageBucket: "my-place-f144b.appspot.com",
+  messagingSenderId: "791571824394",
+  appId: "1:791571824394:web:c1080ff8a4fc294dc144f3",
+  measurementId: "G-MD2BBNPL59"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 createApp(App)
   .use(store)
---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click="login">Sign In</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      login: function() {
+        this.$router.replace('home')
+      }
+    }
+  }
+</script>
+
+<style scoped>  /* "scoped" attribute limit the CSS to this component only */
+  .login {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
