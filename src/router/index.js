a06920acbab24b5ca4f0d31961da7b3ba2bff762
raphaells <raphaelbn10@gmail.com>
2020-10-28 23:14:46 -0300

    login and signup with firebase

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import Login from "../views/Login.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/:catchAll(.*)",
+    component: Login,
+  },
+  {
+    path: "/:/login",
     name: "Login",
     component: Login
   },
@@ -14,7 +18,16 @@ const routes = [
     // this generates a separate chunk (home.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "homr" */ "../views/Home.vue")
+      import(/* webpackChunkName: "home" */ "../views/Home.vue")
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    // route level code-splitting
+    // this generates a separate chunk (sign-up.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "sign-up" */ "../views/SignUp.vue")
   }
 ];
 
